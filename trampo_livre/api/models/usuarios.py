abeba96c5e49cd69653390715db3f5d246a076a2
Jose Claudio <jjoclas@hotmail.com>
2022-04-05 12:52:42 -0300

    fixed senha input

--- trampo_livre/api/models/usuarios.py
+++ trampo_livre/api/models/usuarios.py
@@ -1,11 +1,13 @@
 import uuid
 from django.db import models
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
+
 class Usuarios(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    nome = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    senha = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100, null=False)
+    email = models.EmailField(max_length=100, null=False)
+    senha = models.CharField(max_length=100, null=False)
     telefone = models.CharField(max_length=100)
     data_cadastro = models.DateTimeField(auto_now_add=True)
     data_atualizacao = models.DateTimeField(auto_now=True)
@@ -15,6 +17,7 @@ class Usuarios(models.Model):
         return self.nome
 
 class UsuariosSerializer(serializers.ModelSerializer):
+    senha = serializers.CharField(write_only=True)
     class Meta:
         model = Usuarios
-        fields = ('id', 'nome', 'email', 'telefone', 'data_cadastro', 'data_atualizacao', 'ativo')
+        fields = ('id', 'nome', 'senha', 'email', 'telefone', 'data_cadastro', 'data_atualizacao', 'ativo')
